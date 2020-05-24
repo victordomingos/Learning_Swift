75a61c7f80ead0790e1195b4caf34df5f1b9b486
victordomingos <editor.arcosonline@gmail.com>
2020-05-24 04:10:40 +0100

    hello world

---
+++ clipboard/clipboard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  clipboard
+//
+//  Created by Victor Domingos on 24/05/2020.
+//  Copyright © 2020 Victor Domingos. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
